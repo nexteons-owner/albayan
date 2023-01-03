79cc3722581d105ede4c9eec9fe2d5c847ef1786
MOHAMMAD FAYIZ TP <fayiz1727@gmail.com>
2023-01-03 18:37:35 +0530

    theme set up done

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { buildTheme } from "./assets/global/ThemeVariable";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={buildTheme()}>
+      <CssBaseline />
+
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
