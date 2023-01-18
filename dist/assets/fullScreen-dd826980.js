212487941d161986ed9d6ee5d6ea406ace6ab177
MOHAMMAD FAYIZ TP <fayiz1727@gmail.com>
2023-01-18 14:29:58 +0530

    --fix

---
+++ dist/assets/fullScreen-dd826980.js
@@ -0,0 +1,1 @@
+import{j as t,O as e}from"./index-64fd228e.js";const r=()=>t.jsx(t.Fragment,{children:t.jsx(e,{})});export{r as default};
---
+++ dist/assets/index-72d3c1a2.js
@@ -0,0 +1,1 @@
+import{N as a,j as r}from"./index-64fd228e.js";function n(){const s=a();return r.jsx("h2",{children:JSON.stringify(s,null,2)})}export{n as default};
